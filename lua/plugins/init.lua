6244719f2fd33678a15b68e8cad14bace40d4dbc
Bhoumik Goradia <Bhoumik_Goradia@cable.comcast.com>
2024-02-28 20:29:39 -0500

    keymaps for search/replace

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,4 +2,23 @@ return {
   {
     "ThePrimeagen/vim-be-good",
   },
+  {
+    "nvim-pack/nvim-spectre",
+    keys = {
+      {
+        "<leader>sp",
+        function()
+          require("spectre").open_file_search({ select_word = true })
+        end,
+        desc = "Search/Replace on current file",
+      },
+      {
+        "<leader>sw",
+        function()
+          require("spectre").open_visual({ select_word = true })
+        end,
+        desc = "Search/Replace current word",
+      },
+    },
+  },
 }
