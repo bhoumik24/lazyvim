f534ec185440038499eeb31679ea49008ddba090
Bhoumik Goradia <Bhoumik_Goradia@cable.comcast.com>
2024-02-29 17:01:37 -0500

    added neogen for annotation support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,4 +21,20 @@ return {
       },
     },
   },
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*"
+    keys = {
+      {
+        "<leader>nf",
+        function()
+          require("neogen").generate()
+        end,
+        desc = "Generate",
+      },
+    },
+  },
 }
