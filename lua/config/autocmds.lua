4c34b05cc5e327659f1f01f8ca92faacdb2d1a88
Bhoumik Goradia <bhoumik24@gmail.com>
2025-02-04 12:58:25 -0500

    colorscheme changes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,16 +15,16 @@ vim.api.nvim_create_autocmd("VimLeave", {
   end,
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  desc = "Disable transparent background in Catppuccin when using Neovide",
-  callback = function()
-    if vim.g.neovide then
-      -- Update Catppuccin configuration
-      require("catppuccin").setup({
-        transparent_background = false,
-      })
-      -- Re-apply the colorscheme to activate changes
-      vim.cmd.colorscheme("catppuccin")
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   desc = "Disable transparent background in Catppuccin when using Neovide",
+--   callback = function()
+--     if vim.g.neovide then
+--       -- Update Catppuccin configuration
+--       require("catppuccin").setup({
+--         transparent_background = false,
+--       })
+--       -- Re-apply the colorscheme to activate changes
+--       vim.cmd.colorscheme("catppuccin")
+--     end
+--   end,
+-- })
--- lua/plugins/catppuccin_gb.lua
+++ lua/plugins/catppuccin_gb.lua
@@ -97,7 +97,8 @@ return {
         types = { "bold" },
       },
       flavour = "mocha",
-      transparent_background = vim.g.neovide ~= true,
+      -- transparent_background = vim.g.neovide ~= true,
+      transparent_background = false,
       show_end_of_buffer = false,
       default_integrations = true,
       integrations = {
