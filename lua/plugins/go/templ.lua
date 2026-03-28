8e273f8dd532d0f47be71ad3ff1a5070aa753415
Bhoumik Goradia <bhoumik24@gmail.com>
2026-03-27 20:40:48 -0400

    Fix templ goto definition with Snacks keymap override

--- lua/plugins/go/templ.lua
+++ lua/plugins/go/templ.lua
@@ -1,50 +1,62 @@
 vim.filetype.add({ extension = { templ = "templ" } })
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    ft = "templ",
-    dependencies = "vrischmann/tree-sitter-templ",
+    dependencies = {
+      "vrischmann/tree-sitter-templ",
+    },
+    opts = { ensure_installed = { "templ" } },
   },
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        templ = {},
-        html = {
-          filetypes = { "html", "templ" },
-        },
-        tailwindcss = {
-          filetypes = { "templ", "astro", "javascript", "typescript", "react" },
-          settings = {
-            tailwindCSS = {
-              includeLanguages = {
-                templ = "html",
-              },
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+
+      opts.servers.templ = opts.servers.templ or {}
+      opts.servers.html = vim.tbl_deep_extend("force", opts.servers.html or {}, {
+        filetypes = { "html", "templ" },
+      })
+      opts.servers.tailwindcss = vim.tbl_deep_extend("force", opts.servers.tailwindcss or {}, {
+        filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+        settings = {
+          tailwindCSS = {
+            includeLanguages = {
+              templ = "html",
             },
           },
         },
-      },
-    },
+      })
+
+      local star = opts.servers["*"] or {}
+      star.keys = star.keys or {}
+
+      local filtered = {}
+      for _, key in ipairs(star.keys) do
+        if key[1] ~= "gd" then
+          table.insert(filtered, key)
+        end
+      end
+
+      table.insert(filtered, {
+        "gd",
+        function()
+          return vim.lsp.buf.definition()
+        end,
+        desc = "Goto Definition",
+        has = "definition",
+      })
+
+      star.keys = filtered
+
+      opts.servers["*"] = star
+    end,
   },
   {
     "catgoose/templ-goto-definition",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("templ-goto-definition").setup()
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("TemplGotoDef", { clear = true }),
-        callback = function(ev)
-          local filetype = vim.bo[ev.buf].filetype
-          if filetype == "go" or filetype == "templ" then
-            vim.schedule(function()
-              vim.keymap.set("n", "gd", function()
-                vim.lsp.buf.definition()
-              end, { buffer = ev.buf, desc = "Goto Definition (templ compatible)" })
-            end)
-          end
-        end,
-      })
     end,
     dependencies = "nvim-treesitter/nvim-treesitter",
   },
